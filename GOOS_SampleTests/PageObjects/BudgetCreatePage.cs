22eb144b692f837436e602a4332baa9b7373a485
brett.yu <bagamoon@gmail.com>
2017-12-15 11:50:57 +0800

    add acceptance testing for budget create

---
+++ GOOS_SampleTests/PageObjects/BudgetCreatePage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GOOS_SampleTests.Steps;
+using FluentAutomation;
+
+namespace GOOS_SampleTests.PageObjects
+{
+    public class BudgetCreatePage : PageObject<BudgetCreatePage>
+    {        
+        public BudgetCreatePage(FluentTest test) : base(test)
+        {
+            this.Url = $"{PageContext.Domain}/budget/add";
+        }
+
+        internal BudgetCreatePage Amount(int amount)
+        {
+            I.Enter(amount.ToString()).In("#amount");
+            return this;
+        }
+
+        internal BudgetCreatePage Month(string yearMonth)
+        {
+            I.Enter(yearMonth.ToString()).In("#month");
+            return this;
+        }
+
+        internal void AddBudget()
+        {
+            I.Click("input[type=\"submit\"]");
+        }
+
+        internal void ShouldDisplay(string message)
+        {
+            I.Assert.Text(message).In("#message");
+        }
+    }
+}
